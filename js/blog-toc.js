8b5591b5c83a67f7cccc768aaf5d3b680958bb12
traductik <traductik@traductik.fr>
2017-12-10 23:20:03 +0100

    modifié :         _config.yml 	modifié :         _data/traductions.yml 	modifié :         _includes/blog-right.html 	modifié :         _includes/blog-toc.html 	nouveau fichier : _includes/date-order.html 	nouveau fichier : _includes/date.html 	modifié :         _includes/head.html 	modifié :         _includes/icon-github.html 	modifié :         _includes/icon-twitter.html 	modifié :         _includes/selectors/language-selector-default.html 	modifié :         _includes/selectors/language-selector-page.html 	modifié :         _includes/selectors/language-selector-post.html 	modifié :         _includes/selectors/language-selector-services.html 	modifié :         _includes/selectors/language-selector-welcome.html 	renommé :         _includes/icon-github.svg -> _includes/social-icons/icon-github.svg 	renommé :         _includes/icon-twitter.svg -> _includes/social-icons/icon-twitter.svg 	modifié :         _layouts/default.html 	modifié :         _layouts/post.html 	nouveau fichier : _sass/_blog.scss 	modifié :         _sass/_layout.scss 	modifié :         css/main.scss 	nouveau fichier : js/blog-toc.js

---
+++ js/blog-toc.js
@@ -0,0 +1,16 @@
+
+$('.catlist ul').hide();
+
+$('.catentries').click(function() {
+    $(this).find('ul').slideToggle();
+});
+
+
+$("#shake-em-all").click(function() {
+    $('.catlist ul').slideToggle();
+});
+
+$("#shake-em-all2").click(function() {
+    $('.catlist ul').slideToggle();
+});
+
